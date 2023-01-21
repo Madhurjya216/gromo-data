3e6a424593191bdf4a3c254ae3a2e56bb00ca58e
Madhurjya216 <madhurjyakashyap216@gmail.com>
2023-01-21 21:21:08 +0530

    host api

---
+++ app.js
@@ -0,0 +1,23 @@
+let express = require("express");
+let app = express();
+let cors = require("cors");
+let bondyParser = require("body-parser");
+let product = require("./products.json");
+let category = require("./categories.json");
+let port = process.env.PORT || 9500;
+
+app.use(cors());
+
+
+app.get("/productdata", (req, res) => {
+    res.send(product);
+});
+
+app.get("/categorydata", (req, res) => {
+    res.send(category);
+});
+
+
+app.listen(port, () => {
+    console.log(`server is running in:${port}`)
+})
